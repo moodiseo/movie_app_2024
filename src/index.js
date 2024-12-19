0507efcf65fc6a00d9c0cccd9770054ac34ed020
moodiseo <moodiseo@gmail.com>
2024-12-19 10:57:41 +0900

    #10.1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Potato from "./Potato";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Potato />
   </React.StrictMode>
 );
