5a5818ece9190825cf9cb6e30ff7e98aa974e198
moodiseo <moodiseo@gmail.com>
2025-01-14 11:29:03 +0900

    #14.2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./routes/about";
 import Home from "./routes/home";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
-        <Route path="/about" element={<About />}></Route>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
       </Routes>
     </Router>
   );
