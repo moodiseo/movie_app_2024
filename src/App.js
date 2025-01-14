62ddfa2c841e2539eba9de62d18a147fbed90145
moodiseo <moodiseo@gmail.com>
2025-01-14 12:21:09 +0900

    #14.3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./routes/about";
 import Home from "./routes/home";
 import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/movie-detail" element={<Detail />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/routes/Detail.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Detail(props) {
+  console.log(props);
+  return <div>sdfsdf</div>;
+}
+
+export default Detail;
